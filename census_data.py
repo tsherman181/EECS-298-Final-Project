3f0c4a86a221a8b39069087d0017b36b7e2f910c
tomsherm <tomsherm@umich.edu>
2024-03-29 11:13:50 -0400

    removed variables that required additional weight to simplify project

--- census_data.py
+++ census_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 # Assuming your CSV file is named 'data.csv' and is located in the current directory
-file_path = "Census_Microdata.csv"
+file_path = "Census_Micro.csv"
 
 # Read the CSV file into a Pandas DataFrame
 df = pd.read_csv(file_path)
 
 # Now you can work with the DataFrame 'df'
 print(df.head())
+print(df.columns)
